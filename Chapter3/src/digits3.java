dece9690ec92c2c10b4396755b377a76552f268b
1005218003 <1005218003@L815WD120003.edu.cbe.ab.ca>
2022-03-03 11:13:35 -0700

    Completion

--- Chapter3/src/digits3.java
+++ Chapter3/src/digits3.java
@@ -9,18 +9,20 @@ public class digits3 {
 		Scanner input = new Scanner(System.in); 
 		//record user input.
 		
-		System.out.println("Please enter a two digit number.");
+		System.out.println("Please enter a three digit number.");
 		//asks for a two digit number
 		
 		Digit = input.nextInt();
 		// store input
 		
-		System.out.println("The first number is: " + Digit / 10 );
+		System.out.println("The first number is: " + Digit / 100 );
 		//displays first number by division.
 		
-		System.out.println("The second number is: "+ Digit % 10 );
+		System.out.println("The second number is: "+ Digit % 100 /10 );
 		//display second number via remainder
 		
+		System.out.println("The second number is: "+ Digit % 10 );
+		//display third number number via remainder
 	}
 
 }
