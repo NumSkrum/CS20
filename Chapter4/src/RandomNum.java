d79e53b5af61c58358df6f5796906a4da9b0862f
1005218003 <1005218003@L815WD120003.edu.cbe.ab.ca>
2022-03-09 10:17:12 -0700

    WIP

--- Chapter4/src/RandomNum.java
+++ Chapter4/src/RandomNum.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class RandomNum {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
+		int firstnum, secondnum;
+		
+		Scanner input = new Scanner(System.in); 
+		//record user input.
+		
+		System.out.println("Please enter the first number.");
+		
+		firstnum = input.nextInt();
+		
+		System.out.println("Please enter the second number.");
+		
+		secondnum = input.nextInt();
+		
+		
+		
+		
 	}
 
 }
