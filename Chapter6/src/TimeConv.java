6a7543ef678b8e6c98bbb17b4a6ece3d2453b620
1005218003 <1005218003@L815WD120003.edu.cbe.ab.ca>
2022-03-14 10:09:11 -0600

    Creation of Chapter 6

---
+++ Chapter6/src/TimeConv.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class TimeConv {
+
+	public static void HoursToMinutes() 
+	{
+		int Hours, Min;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of hours.");
+		Hours = input.nextInt();
+		Min = Hours * 60;
+		
+		System.out.println("There are " + Min + " minutes in " +Hours+ " hours.");
+		
+		
+	}
+
+	public static void DaysToHours() 
+	{
+		
+		int Hours, Days;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of days.");
+		Days = input.nextInt();
+		Hours = Days * 24;
+		
+		System.out.println("There are " + Hours + " hours in " + Days + " days.");
+		
+		
+	}
+	public static void main(String[] args)
+	{
+		
+		int choice;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("1.Hours to minutes");
+		System.out.println("2.Days to hours.");
+		
+		choice = input.nextInt();
+		
+		switch(choice)
+		{
+		case 1: HoursToMinutes(); break;
+		
+		case 2: DaysToHours(); break;
+		}
+	
+	}
+	
+}
