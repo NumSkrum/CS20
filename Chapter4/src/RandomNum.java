62230470e9bf3c677cc63a0d0fca44111d18bf17
1005218003 <1005218003@L815WD120003.edu.cbe.ab.ca>
2022-03-15 11:09:35 -0600

    This took way to long.

--- Chapter4/src/RandomNum.java
+++ Chapter4/src/RandomNum.java
@@ -5,20 +5,20 @@ public class RandomNum {
 	public static void main(String[] args) 
 	{
 		
-		int firstnum, secondnum;
+		int firstnum, secondnum, Rando;
 		
 		Scanner input = new Scanner(System.in); 
 		//record user input.
 		
-		System.out.println("Please enter the first number.");
+		System.out.println("Please enter the first number (Minimum).");
 		
 		firstnum = input.nextInt();
 		
-		System.out.println("Please enter the second number.");
+		System.out.println("Please enter the second number (Maximum).");
 		
 		secondnum = input.nextInt();
 		
-		
+		System.out.println((secondnum - firstnum + 1) * Math.random() + firstnum);
 		
 		
 	}
