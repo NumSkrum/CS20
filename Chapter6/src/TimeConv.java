fdcd003d772606287cfec556952aaaec5432c9bc
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-03-29 10:52:16 -0600

    Added comments.

--- Chapter6/src/TimeConv.java
+++ Chapter6/src/TimeConv.java
@@ -9,28 +9,69 @@ public class TimeConv {
 		//Detects input.
 		
 		System.out.println("Please enter the amount of hours.");
-		//
+		//asks for amount of hours
+		
+		Hours = input.nextInt();
+		//stores input
+		
+		Min = Hours * 60;
+		//times minutes by 60
+		
+		System.out.println("There are " + Min + " minutes in " +Hours+ " hours.");
+		//displays output.
+		
+	}
+	public static void HoursToDays() 
+	{
+		int Hours, Days;
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Please enter the amount of hours.");
+		
 		
 		Hours = input.nextInt();
 		
-		Min = Hours * 60;
+		Days = Hours / 24;
+		//divides hours by 24
 		
-		System.out.println("There are " + Min + " minutes in " +Hours+ " hours.");
+		System.out.println("There are " + Days + " day(s) in " +Hours+ " hour(s).");
 		
 		
 	}
-
 	public static void DaysToHours() 
 	{
 		
 		int Hours, Days;
+		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Please enter the amount of days.");
-		Days = input.nextInt();
-		Hours = Days * 24;
+		System.out.println("Please enter the amount of day(s).");
 		
-		System.out.println("There are " + Hours + " hours in " + Days + " days.");
+		Days = input.nextInt();
+		
+		Hours = Days * 24;
+		//times days by 24
+		
+		System.out.println("There are " + Hours + " hour(s) in " + Days + " day(s).");
+		
+		
+	}
+	public static void Minutestohours() 
+	{
+		
+		int Hours, Minutes;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the amount of minute(s).");
+		
+		Minutes = input.nextInt();
+		
+		Hours = Minutes / 60;
+		//divides minutes by 60
+		
+		System.out.println("There are " + Hours + " hour(s) in " + Minutes + " minute(s).");
 		
 		
 	}
@@ -42,14 +83,23 @@ public class TimeConv {
 		
 		System.out.println("1.Hours to minutes");
 		System.out.println("2.Days to hours.");
+		System.out.println("3.Minutes to hours");
+		System.out.println("4.Hours to days");
+		//asks for input
 		
 		choice = input.nextInt();
 		
 		switch(choice)
 		{
 		case 1: HoursToMinutes(); break;
+		//directs users to selected choice.
 		
 		case 2: DaysToHours(); break;
+		
+		case 3: Minutestohours(); break;
+		
+		case 4: HoursToDays(); break;
+		
 		}
 	
 	}
