e288a3e1f75a385b0e8bbd8cacd2e169779d38ab
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-03-30 09:58:10 -0600

    Damn, this Java stuff ain't easy.

--- Chapter6/src/PrimeNumGUI.java
+++ Chapter6/src/PrimeNumGUI.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
 
 public class PrimeNumGUI {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
+		int Num;
+		
+		Scanner input = new Scanner(System.in); 
+		//record user input.
+		
+		System.out.println("Please enter a number.");
+		
+		Num = input.nextInt();
+		
+		if(Num % Num == 0)
+		{
+			
+			System.out.println("This number is a prime number.");
+			
+		}
+		else if (Num / 2 > 0)
+		{
+			
+			System.out.println("This number is not prime Number.");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
