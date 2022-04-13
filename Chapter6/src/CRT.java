159bc1802c07e1aeb124bc5f8c712e141f1887c5
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-04-13 10:16:25 -0600

    Completed Ch6

---
+++ Chapter6/src/CRT.java
@@ -0,0 +1,51 @@
+/*
+2.Method body is where the method happens, declaration is what happens in the body.
+3.Private, Public, and Protected.
+4.UH....
+5.Var 4 and Var 3 can interact in Method1, Var 2 and Var 1 can interact in The main method.
+6.a)public static string getVowel() {
+	Return double
+									}
+  b)public static void extractDigit() {
+	Return double
+										}
+  c)public static void insertString() {
+	Return string
+									}
+7.A) The different name of each method is how the program can tell the difference between the methods
+B) No, The program will not know which Method to pull from when called. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
