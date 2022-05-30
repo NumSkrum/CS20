604a822f71c470667d8cbc2c4cca3bdaf392442e
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-05-30 10:22:33 -0600

    Added Comments.

--- Chapter9/src/GUi/GUi/Squares.java
+++ Chapter9/src/GUi/GUi/Squares.java
@@ -6,16 +6,22 @@ public class Squares {
 
 	public static void main(String[] args) 
 	{
-		final int NUM = 6;
+		final int NUM = 10;
+		//sets Array to 6
 		
 		int[] numbers = new int[NUM];
+		//creates array
+		
 		for(int i = 0; i < NUM; i++)
+		//Loop
 		{
 			numbers[i] = i*i;
+			//times number by itself.
 		}
 		for(int i = 0; i < NUM; i++)
 		{
 			System.out.println(numbers[i]);
+			//prints numbers.
 		}
 	}
 
--- Chapter9/src/GUi/GUi/Studentroster.java
+++ Chapter9/src/GUi/GUi/Studentroster.java
@@ -7,26 +7,34 @@ public class Studentroster {
 	{
 
 		Scanner input = new Scanner(System.in);
+		//Scans input
 		
 		System.out.println("How many students do you have?");
+		//Asks for amount of students.
 		
 		String [] students = new String[input.nextInt()];
+		//Sets amount of students into array
 		
 		System.out.println("Please enter the students name.");
+		//asks for student names
 		
 		for(int i = 0; i < students.length; i++)
+		//Loop
 		{
 			
 			students[i] = input.next();
+			//Gets student names
 			
 			System.out.println("Name has been entered.");
-			
+			//Print name has been entered.
 		}
 		System.out.println("Student roster");
-
+		//Prints Student Roster
+		
 		for(int i = 0; i < students.length; i++)
 		{
 			System.out.println(students[i]);
+			//Prints every name entered
 		}
 		
 	}
