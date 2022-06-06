632544880988692a39c984e1886ff9e1c234b2f5
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-06-06 10:20:19 -0600

    Done

---
+++ Chap7(NoGUI)/src/CRT.java
@@ -0,0 +1,10 @@
+
+public class CRT {
+
+}
+//1. Everything.
+//2. The same as the class name.
+//3. Private means it can't be accessed in other classes. Public means it can.
+//4. No, rzadius can't be 5.
+//5. A) Roo B) x C) public Roo D) SetX E) getX F) Roo G) 4
+//6. Classes create objects, objects use classes.
