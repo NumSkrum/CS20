495d39474a78dfb5df368c984ddb23b5e1d1591a
1005218003 <1005218003@L815WD220006.edu.cbe.ab.ca>
2022-06-07 12:00:08 -0600

    Done.

--- Project9/src/Crt.java
+++ Project9/src/Crt.java
@@ -2,3 +2,12 @@
 public class Crt {
 
 }
+//1.2
+//2.int[] quantities = new int[20];
+//3. double[] heights = new double[input.nextInt()];
+//1.65,2.15,4.85
+//4. int[] grades = new int[input.nextInt()];
+//for(int i = 0; i < grades; i++)
+//{
+//	System.out.println(i + ": " + grades[i]);
+//}
